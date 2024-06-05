ec2a2752ab8468eeeff6dc878e0aed245cca090e
Syméon Smith <symeon.smith@gmail.com>
2024-06-05 13:56:37 +0200

    test: add nested either calls

--- test/discriminated.test.ts
+++ test/discriminated.test.ts
@@ -1,10 +1,16 @@
 import { describe, expect, test } from "bun:test";
 import { validate } from "validator";
 import { isOk } from "validator/result";
-import { either, number, string } from "validator/schema";
+import {
+	boolean,
+	either,
+	number,
+	string,
+	undefinedValue,
+} from "validator/schema";
 
 describe("`either`", () => {
-	test("should validate any of its arguments", () => {
+	test("should only validate its arguments", () => {
 		const res = validate(either(string, number))("hello");
 		expect(isOk(res)).toBe(true);
 
@@ -20,5 +26,20 @@ describe("`either`", () => {
 		expect(isOk(res)).toBe(false);
 		if (isOk(res)) return;
 		expect(res.error.name).toBe("ValidationError");
+	});
+
+	test("should validate nested `either` calls", () => {
+		const schema = either(
+			string,
+			either(number, boolean, either(boolean, undefinedValue))
+		);
+		const res = validate(schema)("hello");
+		expect(isOk(res)).toBe(true);
+
+		const res2 = validate(schema)(undefined);
+		expect(isOk(res2)).toBe(true);
+
+		const res3 = validate(schema)(null);
+		expect(isOk(res3)).toBe(false);
 	});
 });
